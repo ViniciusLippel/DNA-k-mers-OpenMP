b9c4888723b399b290ef89c14d9410b2996c79f8
ViniciusLippel <viniciusmlippel@gmail.com>
2021-12-09 08:41:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
-#include "KmerGenerator.h"
+#include "KGen.cpp"
+#include "FastaReader.cpp"
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 
-int main (int argc, char *argv[]) {
-    KmerGenerator kGen("AAGATGCACGTAGGGTAC", 3);
+const int k = 3;
+
+int main () {
+    
+    FastaReader fasta;
+    std::string dna = fasta.read("dna_files/dna_0.fa");
+    
+    runParallel(dna);
+}
+
+void runParallel(std::string dna) {
+    
+    KGen kGen(dna, k);
     kGen.start();
+   
+    std::list <string> k_list = kGen.getK_mers();
+    
+    printf("\nk-mers: ");
+    for(auto const &i: k_list){
+        std::cout << i << " ";
+    }
+}
+
+void runSerial(std::string dna) {
+
+    for(int i=0; i<=dna.length()-k; i++) {
+
+    }
 }
