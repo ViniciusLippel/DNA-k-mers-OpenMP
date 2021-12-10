3c7293aa8ebd0536ec5faff404bcfbe6a2946520
ViniciusLippel <viniciusmlippel@gmail.com>
2021-12-10 15:12:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "src/FastaReader.cpp"
 #include <stdio.h>
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 const int k = 3;
@@ -10,28 +11,38 @@ const int k = 3;
 void runParallel(std::string dna) {
     
     KGen kGen(dna, k);
+    
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     kGen.start();
-   
-    kGen.printK_mers();
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    
+        std::cout << "\nTime taken: " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << "[ms]" << std::endl;
+    //kGen.printK_mers();
 }
 
 void runSerial(std::string dna) {
 
     KGenSerial kGenSerial(dna, k);
+    
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     kGenSerial.start();
-
-    kGenSerial.printK_mers();
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    
+    std::cout << "\nTime taken: " << std::chrono::duration_cast<std::chrono::milliseconds> (end - begin).count() << "[ms]" << std::endl;
+    //kGenSerial.printK_mers();
+    
 }
 
 int main () {
     
     FastaReader fasta;
-    std::string dna = fasta.read("dna_files/dna_0.fa");
+    std::string dna = fasta.read("dna_files/dna_5.fa");
     
-    std::cout << "Parallel: \n";
+    std::cout << "\nParallel: \n";
     runParallel(dna);
-
+    
     std::cout << "\nSerial: \n";
     runSerial(dna);
+    
 }
 
